589b12eb9ab625fd2da0327e9346ebaf159f63b6
Arruda98 <jeffersonarruda98@gmail.com>
2025-07-09 22:13:19 -0300

    Add files via upload

---
+++ Atividade 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Atividade3
+{
+    class Nome
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é o dia em que você nasceu?");
+            var dia = Console.ReadLine();
+
+            Console.WriteLine("Qual é o mês em que você nasceu?");
+            var mes = Console.ReadLine();
+
+            Console.WriteLine("Qual é o ano em que você nasceu?");
+            var ano = Console.ReadLine();
+
+            Console.WriteLine($"Você nasceu no dia {dia} de {mes} de {ano}, Correto?");
+        }
+    }
+}
