08ec40677032a6ef94679308c1af5c997fd67662
Arruda98 <jeffersonarruda98@gmail.com>
2025-07-09 22:13:46 -0300

    Add files via upload

---
+++ Atividade 4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Atividade4
+{
+    class calculo
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Me fale um número: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Me fala outro número: ");
+            double b = double.Parse(Console.ReadLine());
+
+            double x = a + b;
+
+            Console.WriteLine($"A soma entre {a} e {b} é igua à {x}");
+        }
+    }
+}
