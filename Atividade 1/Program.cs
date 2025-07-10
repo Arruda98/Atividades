a4c1896045dac9a7f3c6e9f93d352d7c59b35911
Arruda98 <jeffersonarruda98@gmail.com>
2025-07-09 22:12:28 -0300

    Add files via upload

---
+++ Atividade 1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Atividade1
+{
+    class Chuva
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Está chovendo?");
+            string primeiraResposta = Console.ReadLine();
+
+            if (primeiraResposta == "Sim")
+            {
+                Console.WriteLine("Você tem um guarda-chuva?");
+                string segundaResposta = Console.ReadLine();
+
+                if (segundaResposta == "Sim")
+                    Console.WriteLine("Saia!");
+
+                else
+                {
+                    Console.WriteLine("Espere um pouco");
+
+                    Thread.Sleep(2000);
+
+                    Console.WriteLine("Está chovendo?");
+                    var terceiraResposta = Console.ReadLine();
+
+                    while (terceiraResposta != "Não")
+                    {
+                        Console.WriteLine("Espere um pouco.");
+
+                        Thread.Sleep(2000);
+
+                        Console.WriteLine("Está chovendo?");
+                        terceiraResposta = Console.ReadLine();
+
+                    }
+                    Console.WriteLine("Saia!");
+                }
+            }
+            else
+                Console.WriteLine("Saia!");
+        }
+    }
+    
+
+}
