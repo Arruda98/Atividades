9fc410d293fe3ccdf3d66b6f97d7b5bdc36a8176
Arruda98 <jeffersonarruda98@gmail.com>
2025-07-09 22:12:54 -0300

    Add files via upload

---
+++ Atividade 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Atividade2
+{
+    class Nome
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("Qual é o seu nome?");
+            var nome = Console.ReadLine();
+
+            Console.WriteLine($"Olá {nome}, prazer em te conhecer!");
+        }
+    }
+}
