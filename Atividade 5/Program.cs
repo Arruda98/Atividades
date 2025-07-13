8a47a0644928cd5f6aad4e6d510a4f53fc4403cb
Arruda98 <jeffersonarruda98@gmail.com>
2025-07-12 22:36:43 -0300

    Add files via upload

---
+++ Atividade 5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Atividade5
+{
+    class Nome
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite algo: ");
+            
+            var x = Console.ReadLine();
+            if(x == typeof(Alpha)) {
+            Console.WriteLine($"O tipo de primitivo desse valor é de: {x}");
+            Console.WriteLine($"É alfabético?" x 
+        }
+    }
+}
---
+++ Atividade 6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Atividade2
+{
+    class Nome
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Quem vai apagar o quadro hoje");
+
+            Console.WriteLine("Primeiro aluno: ");
+            string primeiroAluno = Console.ReadLine();
+            Console.WriteLine("Segundo aluno:");
+            string segundoAluno = Console.ReadLine();
+            Console.WriteLine("Terceiro aluno:");
+            string terceiroAluno = Console.ReadLine();
+            Console.WriteLine("Quarto aluno:");
+            string quartoAluno = Console.ReadLine();
+
+            List<string> alunos = new List<string> 
+            { 
+                primeiroAluno, segundoAluno, terceiroAluno, quartoAluno 
+            };
+
+            Random random = new Random();
+
+            int indiceSorteado = random.Next(alunos.Count);
+
+            Console.WriteLine($"O aluno escolhido foi o: {random}");
+        }
+    }
+}
